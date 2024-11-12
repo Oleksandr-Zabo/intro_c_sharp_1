e2de469628aedb81b1211c980a1a174ebc9628fe
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-11-12 20:43:08 +0200

    ex. 3 lab had done

--- Program.cs
+++ Program.cs
@@ -1,11 +1,47 @@
-﻿namespace Console_sharp_1
+﻿using System.Text;
+namespace Console_sharp_1
 {
-    //dev
+    //lab-3
     public class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            try
+            {
+                int? result = 0;
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.WriteLine($"Введіть {i + 1} цифру: ");
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    
+                    if (number < 0 || number > 9)
+                    {
+                        throw new Exception("Введено не цифру");
+                    }
+                    else
+                    {
+                        if (i == 0)
+                        {
+                            result = number;
+                        }
+                        else
+                        {
+                            result = result * 10 + number;
+                        }
+                    }
+                }
+
+                Console.WriteLine($"Ваше число: {result}");
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Шось пішло не так... : {ex.Message}");
+            } 
         }
     }
 }
+
