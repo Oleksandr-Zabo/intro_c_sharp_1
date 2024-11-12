8a3898c26a2250119f8a71833f6561d4e714a5da
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-11-12 20:30:06 +0200

    ex. 2 had done

--- Program.cs
+++ Program.cs
@@ -1,11 +1,28 @@
-﻿namespace Console_sharp_1
+﻿using System.Text;
+namespace Console_sharp_1
 {
-    //dev
+    //lab-2
     public class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("Введіть число: ");
+            int? number = Convert.ToInt32(Console.ReadLine());
+            number = number ?? 1;
+
+            Console.WriteLine("Введіть відсоток: ");
+
+            int? percent = Convert.ToInt32(Console.ReadLine());
+            percent = percent ?? 1;
+
+            // Ensure number and percent are not null before performing arithmetic
+            double result = (number.GetValueOrDefault() * percent.GetValueOrDefault()) / 100.0;
+
+
+            Console.WriteLine($"Відсоток {percent} від числа {number}: " + result);
         }
     }
 }
