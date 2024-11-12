9ea568369bc3a8af1fe7b68f7be9b863780d73eb
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-11-12 20:20:53 +0200

    ex. 1 lab had done

--- Program.cs
+++ Program.cs
@@ -1,11 +1,36 @@
-﻿namespace Console_sharp_1
+﻿using System.Text;
+namespace Console_sharp_1
 {
-    //dev
+    //lab-1
     public class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("Введіть число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number < 1 || number > 100)
+            {
+                Console.WriteLine("Некорекний діапазон");
+            }
+            else
+            {
+                if (number % 3 == 0)
+                {
+                    Console.Write("Fill");
+                }
+                if (number % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+
+                else if (number % 3 != 0) 
+                {
+                    Console.WriteLine(number);
+                }
+            }
         }
     }
 }
