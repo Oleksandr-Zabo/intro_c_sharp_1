723f31954cfe0b5cd62abde39442fccf9c4effcb
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-11-16 10:02:24 +0200

    ex. 7 lab had done

--- Program.cs
+++ Program.cs
@@ -1,11 +1,44 @@
-﻿namespace Console_sharp_1
+﻿using System.Text;
+namespace Console_sharp_1
 {
-    //dev
+    //lab-6
     public class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            try
+            {
+                Console.WriteLine("Введіть перше число: ");
+                int number_1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Введіть друге число: ");
+                int number_2 = Convert.ToInt32(Console.ReadLine());
+
+                if (number_1 > number_2) { 
+                    int temp = number_1;
+                    number_1 = number_2;
+                    number_2 = temp;
+                }
+
+                for (int i = number_1; i <= number_2; i++)
+                {
+                    if (i % 2 == 0) {
+                        Console.Write(i + " ");
+                    }
+                    
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Помилка: {ex}");
+            }
+
+
         }
     }
 }
