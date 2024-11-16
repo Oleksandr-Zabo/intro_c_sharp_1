fbf4b4565aafc1d40dc49085c05b4c204aee819d
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-11-16 10:19:58 +0200

    ex. 3 hw had done

--- Program.cs
+++ Program.cs
@@ -1,11 +1,51 @@
-﻿namespace Console_sharp_1
+﻿using System.Text;
+namespace Console_sharp_1
 {
-    //dev
+    //hw-3
     public class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            try
+            {
+                Console.WriteLine("Введіть нижню межу діапазону:");
+                int lowerBound = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Введіть верхню межу діапазону:");
+                int upperBound = Convert.ToInt32(Console.ReadLine());
+
+                if (lowerBound > upperBound) {
+                    throw new Exception("Нижня межа діапазону не може бути більшою за верхню межу діапазону");
+                }
+
+                int a = 0;
+                int b = 1;
+
+                Console.WriteLine($"Числа Фібоначчі в діапазоні від {lowerBound} до {upperBound}:");
+
+                while (a <= upperBound)
+                {
+                    if (a >= lowerBound)
+                    {
+                        Console.Write(a + " ");
+                    }
+
+                    int temp = a;
+                    a = b;
+                    b = temp + b;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Помилка: {ex}");
+            }
+            
         }
     }
 }
+
