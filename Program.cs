e88677ed93dfe37d1f50c45d22c6386fd9f89efa
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-11-16 10:12:56 +0200

    ex. 2 hw had done

--- Program.cs
+++ Program.cs
@@ -1,11 +1,48 @@
-﻿namespace Console_sharp_1
+﻿using System.Text;
+namespace Console_sharp_1
 {
-    //dev
+    //hw-2
     public class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            int? number;
+            int? sum = 0;
+            int? max = 0;
+            int? min = 0;
+            int? product = 1;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Введіть число {i + 1}: ");
+                number = Convert.ToInt32(Console.ReadLine());
+                sum += number;
+                if (i == 0)
+                {
+                    max = number;
+                    min = number;
+                    product = number;
+                }
+                else
+                {
+                    if (number > max)
+                    {
+                        max = number;
+                    }
+                    if (number < min)
+                    {
+                        min = number;
+                    }
+                    product *= number;
+                }
+            }
+            
+            Console.WriteLine($"Сума: {sum}");
+            Console.WriteLine($"Максимальне: {max}");
+            Console.WriteLine($"Мінімальне: {min}");
+            Console.WriteLine($"Добуток: {product}");
         }
     }
 }
