8db9c3131a68af12da2eb3ab7cc2aef53e1debdd
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-11-16 10:28:38 +0200

    ex.5 hw had done

--- Program.cs
+++ Program.cs
@@ -1,11 +1,42 @@
-﻿namespace Console_sharp_1
+﻿using System.Text;
+namespace Console_sharp_1
 {
-    //dev
+    //hw-5
     public class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            try
+            {
+                Console.WriteLine("Введіть нижню межу діапазону:");
+                int lowerBound = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Введіть верхню межу діапазону:");
+                int upperBound = Convert.ToInt32(Console.ReadLine());
+
+                if (lowerBound > upperBound)
+                {
+                    throw new Exception("Нижня межа діапазону не може бути більшою за верхню межу діапазону");
+                }
+                for (int i = lowerBound; i <= upperBound; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.Write(i + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Помилка: {ex}");
+            }
+
         }
     }
 }
