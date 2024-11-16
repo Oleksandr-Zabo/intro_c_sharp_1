7216c82c8313df1d1cb6998069f43ada98103a2b
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-11-16 09:58:13 +0200

    ex. 6 lab had done

--- Program.cs
+++ Program.cs
@@ -1,11 +1,37 @@
-﻿namespace Console_sharp_1
+﻿using System.Text;
+namespace Console_sharp_1
 {
-    //dev
+    //lab-6
     public class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            try
+            {
+                Console.WriteLine("Введіть температуру: ");
+                double number = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Введіть тип конвертації (0- градуси Цельсія в фаренгейт; 1- Фаренгейт в градуси Цельсія) : ");
+                bool type = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
+
+                if (type)
+                {
+                    Console.WriteLine("Температура в градусах Цельсія: " + ((number - 32) * 5 / 9));
+                }
+                else
+                {
+                    Console.WriteLine("Температура в градусах Фаренгейта: " + (number * 9 / 5 + 32));
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Помилка: {ex}");
+            }
+
+            
         }
     }
 }
