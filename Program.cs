e0b42f4b2cdd8696ec91aae9eb2558ffb39e1be6
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-11-16 10:06:43 +0200

    ex. 1 hw had done

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿namespace Console_sharp_1
 {
-    //dev
+    //hw-1
     public class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("It's easy to win forgiveness for being wrong; \nbeing right is what gets you into real trouble. \nBjarne Stroustrup");
         }
     }
 }
